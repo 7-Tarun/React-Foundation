ac4a54335d1ce1aa7aaf5026338ef3ab259ba971
7-Tarun <tarunghadoria890@gmail.com>
2026-05-26 09:59:28 +0530

    learning basics components, import-export, function return etc

---
+++ react-playground/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+//Creating a NavBar in a seperate file.
+
+function Navbar() {
+    return(
+        <nav>
+            <h2>Hello World</h2>
+            <ul>
+                <li>Home</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
+
+//The next step is to add this file in App.jsx file.
