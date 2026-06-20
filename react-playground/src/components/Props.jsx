b54f66008484d6d28c89ddef566a124f61f2eb5c
7-Tarun <tarunghadoria890@gmail.com>
2026-06-20 10:59:10 +0530

    minor change

--- react-playground/src/components/Props.jsx
+++ react-playground/src/components/Props.jsx
@@ -18,6 +18,7 @@ export default Props
 The Problem: Repetitive CodeWriting (props.userName, props.cartCount, and props.theme) repeatedly makes your code long, messy, and ugly when dealing with multiple props.
 The Solution: Destructuring (The Industry Standard)
 Since props are just JavaScript objects behind the scenes, you can use ES6 Object Destructuring to unpack the variables directly inside the function parameters.
+
 Example:- function Navbar({ userName, cartCount }) {
   return <h1>{userName}</h1>;
 } */
