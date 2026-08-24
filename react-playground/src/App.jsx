af972664f6eda37d7db7d441877c2ed243dd3633
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-08-24 18:37:28 +0530

    learn: practice React components

---
+++ react-playground/src/App.jsx
@@ -0,0 +1,13 @@
+import JsxBasics from './React-Basics/01_Basics/JsxBasic'
+
+function App() {
+    return (
+        <div>
+            <JsxBasics/>
+        </div>
+    )
+}
+
+export default App
+
+//Execution Flow: index.html  →  main.jsx  →  App.jsx  →  JsxBasics.jsx
---
+++ react-playground/src/React-Basics/01_Basics/JsxBasic.jsx
@@ -0,0 +1,12 @@
+function JsxBasics() {
+    const name = "Stephen";
+    return (
+        <div>
+            <h1>Hello I am React</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, totam.</p>
+            <p>I am {name}</p>
+        </div>
+    )
+}
+
+export default JsxBasics
