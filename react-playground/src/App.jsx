40e7568ba38682017c9885a8bf205aa6399c4d4d
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-01 18:46:06 +0530

    new component imported to the App.jsx

--- react-playground/src/App.jsx
+++ react-playground/src/App.jsx
@@ -1,11 +1,17 @@
 import JsxBasics from './React-Basics/01_Basics/JsxBasic'
+import Greeting from '.React-Basics/02_Components/Greeting'
 
 function App() {
     return (
         <div>
             <JsxBasics/>
+            <Greeting name = "Master" age = {21}/>
         </div>
     )
 }
 
 export default App
+
+// Props are read-only and cannot be modified by the child component.
+// React follows one-way data flow: data moves from parent to child.
+// This makes data flow predictable and easier to manage.
