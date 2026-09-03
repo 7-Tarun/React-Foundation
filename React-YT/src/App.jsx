c1ba09347fe0cf95dc5b5a72fc275e4e727b8c64
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-03 20:12:06 +0530

    learn- hooks basics

--- React-YT/src/App.jsx
+++ React-YT/src/App.jsx
@@ -1,9 +1,25 @@
 function App() {
+  let counter = 5;
+
+  const addValue = () => {
+    console.log("Value added: ", counter);
+    counter = counter+1;
+    
+  }
+
   return (
     <>
       <h1>Hello World!</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add   {counter}</button>
+      <button>Remove</button>
+      <p>{counter}</p>
     </>
   )
 }
 
 export default App
+
+//UI Updation is controlled by React
