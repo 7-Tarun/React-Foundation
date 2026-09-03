cbc3938f344afb366236e461e40b655c4e2c9ace
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-03 19:07:25 +0530

    basic react bolier plate created

--- React-YT/src/App.jsx
+++ React-YT/src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-
 function App() {
-  const username = "Master";
   return (
-    <div>
-      <p>Hello {username}</p>
+    <>
       <h1>Hello World!</h1>
-    </div>
+    </>
   )
 }
 
