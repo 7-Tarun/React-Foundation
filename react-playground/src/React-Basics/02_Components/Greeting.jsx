a4035d169835d620cd8d9bd6f089bb473f1e1e5e
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-04 14:00:16 +0530

    benefits of detructuring props documented

--- react-playground/src/React-Basics/02_Components/Greeting.jsx
+++ react-playground/src/React-Basics/02_Components/Greeting.jsx
@@ -12,4 +12,8 @@ export default Greeting
 
 // Props are read-only and cannot be modified by the child component.
 // React follows one-way data flow: data moves from parent to child.
-// This makes data flow predictable and easier to manage.
+
+// What's the practical benefit of destructuring props, other than just shorter code?
+// -Readability:      function UserCard({ name, age, email, avatar }) {  // yehi pata chal gaya kya-kya chahiye
+// -You can set default values:    function Greeting({ name, age = 18 }) {   // age nahi diya toh 18 default
+// -This makes data flow predictable and easier to manage.
