8126b58e34c0f2b4bdeb4bc285e6d337d89615f6
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-09 16:24:33 +0530

    color project buttons added

--- React-YT/src/App.jsx
+++ React-YT/src/App.jsx
@@ -1,12 +1,14 @@
 import Counter from './Components/Counter'
 import Card from './Components/Card'
+import PjColor from './Projects/1_bg-Changer'
 
 function App() {
 
   return (
     <>
-      <Counter />
-      <Card />
+      {/* <Counter /> */}
+      {/* <Card /> */}
+      <PjColor/>
     </>
   )
 }
--- React-YT/src/Projects/1_bg-Changer.jsx
+++ React-YT/src/Projects/1_bg-Changer.jsx
@@ -1,12 +1,33 @@
 import { useState } from "react";
 
-function Color() {
+function PjColor() {
 
-    const [] = useState("White");
+    const [color, setColor] = useState("grey");
 
     return(
-        <>
-        
-        </>
+        <div className="w-full h-screen duration-200" style={{background: color}}>
+            <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+                <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-amber-50 px-2 py-2 rounded-full">
+
+                    <button onClick={() => setColor("red")}
+                    className="outline-none px-3 text-white text-xl rounded-lg shadow-xl" style={{backgroundColor: "red"}}>Red</button>
+                    <button onClick={() => {setColor("green")}} 
+                    className="outline-none px-3 text-white text-xl rounded-lg shadow-xl" style={{backgroundColor: "green"}}>Green</button>
+                    <button onClick={() => setColor("blue")} 
+                    className="outline-none px-3 text-white text-xl rounded-lg shadow-xl" style={{backgroundColor: "blue"}}>Blue</button>
+                    <button onClick={() => setColor("purple")} 
+                    className="outline-none px-3 text-white text-xl rounded-lg shadow-xl" style={{backgroundColor: "purple"}}>Purple</button>
+                    <button onClick={() => setColor("orange")} 
+                    className="outline-none px-3 text-white text-xl rounded-lg shadow-xl" style={{backgroundColor: "orange"}}>Orange</button>
+                    <button onClick={() => setColor("black")} 
+                    className="outline-none px-3 text-white text-xl rounded-lg shadow-xl" style={{backgroundColor: "black"}}>Black</button>
+                    <button onClick={() => setColor("bisque")} 
+                    className="outline-none px-3 text-white text-xl rounded-lg shadow-xl" style={{backgroundColor: "bisque"}}>Bisque</button>
+                    
+                </div>
+            </div>
+        </div>
     )
 }
+
+export default PjColor
