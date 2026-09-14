45c068a35557fd95d605a35122fbf0e279493d42
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-14 20:12:51 +0530

    1st React Router created

--- React-playground2/src/App.jsx
+++ React-playground2/src/App.jsx
@@ -1,12 +1,12 @@
-function App() {
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
+function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   )
 }
-
-export default App
