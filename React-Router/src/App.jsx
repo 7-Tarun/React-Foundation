d8eb506d6f9815cd26356d10b6bb1c730371547c
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-16 17:06:16 +0530

    Image imported and button functionality added

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -1,6 +1,9 @@
+import { Route, Routes} from "react-router-dom"
+
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
 import Home from "./Components/Home"
+import About from "./Components/About"
 
 function App() {
 
--- React-Router/src/Components/Home.jsx
+++ React-Router/src/Components/Home.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import Image from '../assets/Jungle.png';
+import Image2 from '../assets/Rakoon.png';
 
 export default function Home() {
     return (
@@ -30,13 +32,13 @@ export default function Home() {
                     </div>
                 </div>
 
-                <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full">
+                    <img className="w-96" src={Image2} alt="image1" />
                 </div>
             </aside>
 
             <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+                <img className="sm:w-96 w-48" src={Image} alt="image2" />
             </div>
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
