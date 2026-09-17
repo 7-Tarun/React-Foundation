831363dc368ac972d4098253279f9506a85c542c
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-17 13:02:36 +0530

    github data updated

--- React-Router/src/Components/Github.jsx
+++ React-Router/src/Components/Github.jsx
@@ -1,18 +1,18 @@
 import { useState, useEffect } from "react";
 
 function Github() {
-    const[data,setData] = useState({});
+    const [data, setData] = useState({});
 
     useEffect(() => {
         const fetchData = async () => {
-            try{
+            try {
                 const url = "https://api.github.com/users/7-Tarun";
                 const response = await fetch(url);
                 const resData = await response.json();
-                
+
                 setData(resData);
             }
-            catch(error){
+            catch (error) {
                 alert("Error Occured: Can't fetch the data plz don't try again");
             }
         }
@@ -21,11 +21,24 @@ function Github() {
 
     }, [])
 
-    return(
+    return (
         <>
-        <div className="text-center text-3xl text-white m-4 p-4 bg-gray-400">
-            Github Followers: {data.followers}
-        </div>
+            <div className=" text-start rounded-xl text-3xl text-white m-3 p-3 bg-gray-600">
+                <span className="text-black">Github User Name:</span>{" "}{data.login}
+            </div>
+            <div className="text-start rounded-xl text-3xl text-white m-3 p-3 bg-gray-600">
+                <span className="text-black">Github Profile:</span>{" "}{data.html_url}
+            </div>
+            <div className="text-start rounded-xl text-3xl text-white m-3 p-3 bg-gray-600">
+                <span className="text-black">Github Followers:</span>{" "}{data.followers} 🥀
+            </div>
+
+            <div className=" flex text-start rounded-xl text-3xl text-white m-3 p-3 bg-gray-600">
+                <img className="rounded-xl" src={data.avatar_url} width={200} height={200} alt="Git Picture" />
+                <div className=" ml-5 ">
+                    <span className="text-black">Github Bio:</span>{" "} {data.bio}
+                </div>
+            </div>
         </>
     )
 }
