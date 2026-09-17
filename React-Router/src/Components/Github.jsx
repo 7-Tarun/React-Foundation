a8a644658ba80649b73cbc240aea70f1e739855e
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-17 12:09:05 +0530

    data fetch from github

--- React-Router/src/Components/Github.jsx
+++ React-Router/src/Components/Github.jsx
@@ -1,14 +1,30 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react";
 
 function Github() {
+    const[data,setData] = useState({});
+
     useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const url = "https://api.github.com/users/7-Tarun";
+                const response = await fetch(url);
+                const resData = await response.json();
+                
+                setData(resData);
+            }
+            catch(error){
+                alert("Error Occured: Can't fetch the data plz don't try again");
+            }
+        }
+
+        fetchData();
 
     }, [])
 
     return(
         <>
         <div className="text-center text-3xl text-white m-4 p-4 bg-gray-400">
-            Github Followers: 
+            Github Followers: {data.followers}
         </div>
         </>
     )
