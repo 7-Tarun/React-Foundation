e6a9c648db8d68db0230699d55568611bde3c50f
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-19 12:15:06 +0530

    Context Api React Project Created

---
+++ Context-Api/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline text-center text-blue-100 bg-blue-600">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
