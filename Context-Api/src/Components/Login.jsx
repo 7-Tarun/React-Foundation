e6533cffd44ebfe57f2b0747cee3d0931a243207
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-19 18:25:17 +0530

    learn: implement UserContext and ContextProvider for global state management

---
+++ Context-Api/src/Components/Login.jsx
@@ -0,0 +1,25 @@
+import { useState, useContext } from "react"
+import UserContext from "../Context/UserContext"
+
+function Login() {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {setUser} = useContext(UserContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUser({userName, password})
+    }
+
+    return(
+        <>
+        <h2>Login</h2>
+        <input value={userName} onChange={(e) => setUserName(e.target.value)} type="text" placeholder="UserName" />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="text" placeholder="Password" />
+        <button onClick={handleSubmit}>Submit</button>
+        </>
+    )
+}
+
+export default Login
