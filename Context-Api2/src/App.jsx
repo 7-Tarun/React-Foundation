42ebb7ca4dffdd2c6a37ca7cb338ed50c0b0d8d6
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-21 10:33:02 +0530

    feat: implement theme toggle  and Card and Themebtn components

--- Context-Api2/src/App.jsx
+++ Context-Api2/src/App.jsx
@@ -1,19 +1,38 @@
+import { useEffect, useState } from "react"
+import { ThemeProvider } from "./Context/Theme"
+import Themebtn from "./Components/Themebtn";
+import Card from "./Components/Card";
+
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
 
+  const lightTheme = () => {
+    setThemeMode("light");
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light","dark");
+    document.querySelector('html').classList.add(themeMode);
+  }, [themeMode]);
+  
   return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <Themebtn/>
+          </div>
 
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* Theme */}
-        </div>
-
-        <div className="w-full max-w-sm mx-auto">
-          {/* Card */}
+          <div className="w-full max-w-sm mx-auto">
+            <Card/>
+          </div>
         </div>
       </div>
-    </div>
-
+    </ThemeProvider>
   )
 }
 
