e77cad1f5f4a7bc226d1e2232825783a2fc8c357
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-23 14:57:26 +0530

    File renamed and new react vite project created

--- 6_Context-Api/src/App.jsx
+++ 6_Context-Api/src/App.jsx
@@ -1,38 +1,13 @@
-import { useEffect, useState } from "react"
-import { ThemeProvider } from "./Context/Theme"
-import Themebtn from "./Components/Themebtn";
-import Card from "./Components/Card";
+import Login from "./Components/Login"
+import Profile from "./Components/Profile"
 
 function App() {
-  const [themeMode, setThemeMode] = useState("light");
 
-  const lightTheme = () => {
-    setThemeMode("light");
-  }
-
-  const darkTheme = () => {
-    setThemeMode("dark");
-  }
-
-  useEffect(() => {
-    document.querySelector('html').classList.remove("light","dark");
-    document.querySelector('html').classList.add(themeMode);
-  }, [themeMode]);
-  
   return (
-    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            <Themebtn/>
-          </div>
-
-          <div className="w-full max-w-sm mx-auto">
-            <Card/>
-          </div>
-        </div>
-      </div>
-    </ThemeProvider>
+    <>
+    <Login/>
+    <Profile/>
+    </>
   )
 }
 
