b2cdc90aabbb16ca702616e8a6f734b56d780ecc
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-24 11:32:38 +0530

    feat: create Card component with theme toggle functionality

--- 6_Context-Api/src/App.jsx
+++ 6_Context-Api/src/App.jsx
@@ -1,26 +1,13 @@
-import { useTheme } from "./Components/UseTheme"
-import { useState } from "react"
-// import IncidentReportCard from '@/components/ui/area-chart-1'; 
+// import { useTheme } from "./Components/UseTheme"
+// import { useState } from "react"
+import Card from "./Components/Card"
 
 function App() {
-  const {theme, toggleTheme} = useTheme();
+  // const { theme, toggleTheme } = useTheme();
 
   return (
     <>
-    <div className="card bg-base-100 w-80 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+    <Card/>
     </>
   )
 }
---
+++ 6_Context-Api/src/Components/Card.jsx
@@ -0,0 +1,31 @@
+import { useTheme } from "./UseTheme"
+import { useContext } from "react"
+
+
+function Card() {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <main className="page-shell">
+            <div className={`card product-card ${theme === 'dark' ? 'bg-gray-900' : 'bg-gray-100'}`}>
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className={`card-title ${theme === "dark" ? 'text-green-100' : 'text-green-800'}`}>Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                        <button 
+                        className="btn btn-primary"
+                        onClick={toggleTheme}
+                        >Toggle Theme</button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Card
