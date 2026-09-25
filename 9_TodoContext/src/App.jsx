d1f74eea0cdbc867c180be3f220b2353ff32f0f3
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-25 18:20:31 +0530

    update: add deleteTodo and toggleComplete functions in App component

--- 9_TodoContext/src/App.jsx
+++ 9_TodoContext/src/App.jsx
@@ -13,6 +13,13 @@ function App() {
     setTodos((prev)=> prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
   }
 
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !==id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  }
 
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
