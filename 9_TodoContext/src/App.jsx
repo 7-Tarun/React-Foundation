25e50c28253bb64dcfa9c2f5efb447a3bda5607a
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-26 19:33:15 +0530

    feat: integrate TodoForm and TodoItem components for enhanced todo management

--- 9_TodoContext/src/App.jsx
+++ 9_TodoContext/src/App.jsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react"
 import {TodoProvider} from './contexts/index'
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
 
   const [todos,setTodos] = useState([]);
 
   const addTodo = (todo) => {
-    setTodos((prev) => [{id: Date.now(),todo},...prev])
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev])
   }
 
-  const updateTodo = (id,todo) => {
-    setTodos((prev)=> prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
   }
 
   const deleteTodo = (id) => {
@@ -40,9 +42,16 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
             {/* Todo form goes here */}
+            <TodoForm/>
           </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id}
+              className="w-full">
+                <TodoItem todo={todo}/>
+              </div>
+            ))}
           </div>
         </div>
       </div>
